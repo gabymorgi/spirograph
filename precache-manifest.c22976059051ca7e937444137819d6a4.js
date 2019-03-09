7dd57b45d21f5b0aaef4f9e3d6eb4246b233b720
GabyMorgi <gabymorgi@gmail.com>
2019-03-09 14:35:09 -0300

    Updates

---
+++ precache-manifest.c22976059051ca7e937444137819d6a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "882bc4bdd4e0f1d59d7e",
+    "url": "/spirograph/static/js/runtime~main.882bc4bd.js"
+  },
+  {
+    "revision": "d13b28cd9cb16d2f2214",
+    "url": "/spirograph/static/js/main.d13b28cd.chunk.js"
+  },
+  {
+    "revision": "46dd98477796c5031c55",
+    "url": "/spirograph/static/js/2.46dd9847.chunk.js"
+  },
+  {
+    "revision": "d13b28cd9cb16d2f2214",
+    "url": "/spirograph/static/css/main.e39a9272.chunk.css"
+  },
+  {
+    "revision": "46dd98477796c5031c55",
+    "url": "/spirograph/static/css/2.f81d9e56.chunk.css"
+  },
+  {
+    "revision": "18be97d26aeb67f9073f01f8098d223b",
+    "url": "/spirograph/index.html"
+  }
+];
